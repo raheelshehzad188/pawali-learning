805bcc1dafb7e8cdc2c2370f65abf853b5f9645f
raheelshehzad188 <raheelshehzad188@gmail.com>
2023-12-14 00:56:18 +0500

    select function

--- function.php
+++ function.php
@@ -23,4 +23,38 @@ if ($result->num_rows > 0) {
     return false;
 }
 }
+function select($tbl,$where = array())
+{
+    $conn = get_conn();
+    $wh = '';
+    foreach ($where as $k=> $v)
+    {
+        if(!$wh)
+        {
+            $wh = $wh." ".$k." = '".$v."'";
+        }
+        else
+        {
+            $wh = $wh." AND ".$k." = '".$v."'";
+        }
+    }
+    $sql = "SELECT * FROM ".$tbl;
+    if($wh)
+    $sql = "SELECT * FROM ".$tbl." where ".$wh;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $data = array();
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    return  $data;
+} else {
+    return array();
+}
+}
+function khawar_sum($a, $b)
+{
+    return $a+$b;
+}
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include "function.php";
+$wh = array('email'=>'admin@admin.com','pass'=>'admin');
+$all = select('admins',$wh);
 $user = admin_login('admin@admin.com','admin');
-var_dump($user);
+$r = khawar_sum(2,5);
+var_dump($all);
 ?>
